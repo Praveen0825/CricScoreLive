597d67203a17db6d7945a7af21b801ce800e06f4
Praveen1208 <91229praveenkumar@gmail.com>
2021-01-08 12:26:21 +0530

    first commit

---
+++ src/api/Api.js
@@ -0,0 +1,23 @@
+const API_KEY="TAfFhOIibgd7i6TsEz7jbBIwm4o2";
+
+
+export const getMatches = () => {
+    const url = `https://cricapi.com/api/matches?apikey=${API_KEY}`;
+    console.log("URL", url);
+    return fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("ERROR ", error);
+      });
+  };
+  
+  //get the score of the cuurent match
+  
+  export const getMatchDetail = (id) => {
+    const url = `https://cricapi.com/api/cricketScore?unique_id=${id}&apikey=${API_KEY}`;
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
+  };
